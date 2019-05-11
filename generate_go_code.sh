33d9e4b4c29ff03a3864af1608a382528a457a5f
Piotr Kardaś <pkardas.it@gmail.com>
2019-05-11 22:32:45 +0200

    [3] Added classes proto

--- generate_go_code.sh
+++ generate_go_code.sh
@@ -2,3 +2,5 @@
 
 protoc protos/user.proto  --go_out=plugins=grpc:generated
 protoc protos/chat.proto  --go_out=plugins=grpc:generated
+protoc protos/status.proto  --go_out=plugins=grpc:generated
+protoc protos/class.proto  --go_out=plugins=grpc:generated
